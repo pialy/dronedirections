9d740eed47bcd1f8bf276c614a1b3d3232a6f96c
lolivier <lolivier@et.esiea.fr>
2023-05-31 16:15:17 +0200

    add 2 fic

---
+++ a.py
@@ -0,0 +1,27 @@
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+from djitellopy import Tello
+
+kivy.require('2.1.0')
+
+class CameraClick(BoxLayout):
+    def __init__(self, **kwargs):
+        super(CameraClick, self).__init__(**kwargs)
+        self.tello = Tello()
+        self.tello.connect()
+        self.tello.streamon()
+
+    def capture(self):
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+
+class atest(App):
+    def build(self):
+        camera_click = CameraClick(drone=self.tello)
+        return camera_click
+
+testApp = atest()
+testApp.run()
--- main.py
+++ main.py
@@ -1,70 +1,72 @@
 #/usr/bin/python
-
-# import socket
-
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# tello_adress = ('192.168.10.1', 8889)
-# sock.bind(('',9000))
-
-# while True:
-#     try:
-#         msg = input('')
-#         if not msg:
-#             break
-#         if 'end' in msg:
-#             sock.close()
-#             break
-#         msg = msg.encode()
-#         print(msg)
-#         sent = sock.sendto(msg, tello_adress)
-#     except Exception as err:
-#         print(err)
-#         sock.close()
-#         break
-
 from djitellopy import tello
 import time
 import cv2
 import threading
 from threading import Thread, Event
+import socket
 
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
-me.streamoff()
-me.streamon()
-me.takeoff()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tello_adress = ('192.168.10.1', 8889)
+sock.bind(('',9000))
 
-def rotation():
-        me.rotate_clockwise(90)
-        time.sleep(2)
-
-
-
-def camera():    
-    while True:
-        img = me.get_frame_read().frame
-        img = cv2.resize(img, (640, 480))
-        cv2.imshow("Image", img)
-        
-        if cv2.waitKey(5) & 0xFF == ord('q'):
+while True:
+    try:
+        msg = input('')
+        if not msg:
             break
+        if 'end' in msg:
+            sock.close()
+            break
+        msg = msg.encode()
+        print(msg)
+        sent = sock.sendto(msg, tello_adress)
+    except Exception as err:
+        print(err)
+        sock.close()
+        break
 
 
-started_evt = Event()
-y = threading.Thread(target=camera)
-x = threading.Thread(target=rotation)
+
+# me = tello.Tello()
+# me.connect()
+# print(me.get_battery())
+# me.streamoff()
+# me.streamon()
+# me.takeoff()
+# me.move_up(20)
+
+# def rotation():
+#         me.rotate_clockwise(90)
+#         time.sleep(2)
 
 
-y.start()
-time.sleep(4)
 
-for i in range(8):
-    me.rotate_clockwise(45)
-    time.sleep(1)
+# def camera():    
+#     while True:
+#         img = me.get_frame_read().frame
+#         img = cv2.resize(img, (640, 480))
+#         cv2.imshow("Image", img)
+        
+#         if cv2.waitKey(5) & 0xFF == ord('q'):
+#             break
+
+
+# started_evt = Event()
+# y = threading.Thread(target=camera)
+# x = threading.Thread(target=rotation)
+
+
+# y.start()
+# time.sleep(4)
+
+
+# for i in range(8):
+#     me.rotate_clockwise(45)
+#     time.sleep(1)
     
 
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
 
 
 
