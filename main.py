3fbdb301add93239a8b421250fe7cfc01b108f82
lolivier <lolivier@et.esiea.fr>
2023-05-31 16:26:56 +0200

    replace main.py

--- main.py
+++ main.py
@@ -1,72 +1,79 @@
 #/usr/bin/python
+
+# import socket
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# tello_adress = ('192.168.10.1', 8889)
+# sock.bind(('',9000))
+
+# while True:
+#     try:
+#         msg = input('')
+#         if not msg:
+#             break
+#         if 'end' in msg:
+#             sock.close()
+#             break
+#         msg = msg.encode()
+#         print(msg)
+#         sent = sock.sendto(msg, tello_adress)
+#     except Exception as err:
+#         print(err)
+#         sock.close()
+#         break
+
 from djitellopy import tello
 import time
 import cv2
 import threading
 from threading import Thread, Event
-import socket
+#from production.linedetection.src.scripts.lines import detect_line
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-tello_adress = ('192.168.10.1', 8889)
-sock.bind(('',9000))
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+me.streamoff()
+me.streamon()
+me.takeoff()
 
-while True:
-    try:
-        msg = input('')
-        if not msg:
-            break
-        if 'end' in msg:
-            sock.close()
-            break
-        msg = msg.encode()
-        print(msg)
-        sent = sock.sendto(msg, tello_adress)
-    except Exception as err:
-        print(err)
-        sock.close()
-        break
+def rotation():
+        me.rotate_clockwise(90)
+        time.sleep(2)
 
 
 
-# me = tello.Tello()
-# me.connect()
-# print(me.get_battery())
-# me.streamoff()
-# me.streamon()
-# me.takeoff()
-# me.move_up(20)
-
-# def rotation():
-#         me.rotate_clockwise(90)
-#         time.sleep(2)
-
-
-
-# def camera():    
-#     while True:
-#         img = me.get_frame_read().frame
-#         img = cv2.resize(img, (640, 480))
-#         cv2.imshow("Image", img)
+def camera():    
+    n = 0
+    while True:
+        img = me.get_frame_read().frame
+        img = cv2.resize(img, (640, 480))
+        cv2.imshow("Image", img)
+        # if n == 10 :    
+        #     #print("Executing ...")
+        #     r = detect_line(img, 1)
+        #     cv2.imshow("Image", r)
+        #     n = 0
+        # n += 1
         
-#         if cv2.waitKey(5) & 0xFF == ord('q'):
-#             break
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
 
 
-# started_evt = Event()
-# y = threading.Thread(target=camera)
-# x = threading.Thread(target=rotation)
+started_evt = Event()
+y = threading.Thread(target=camera)
+x = threading.Thread(target=rotation)
 
 
-# y.start()
-# time.sleep(4)
-
+y.start()
+time.sleep(4)
+me.move_forward(20)
 
 # for i in range(8):
 #     me.rotate_clockwise(45)
 #     time.sleep(1)
     
 
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
 
 
 
