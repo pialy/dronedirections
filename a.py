f64e57207ca6bfe1e5c0609755d813233ce1f6f3
lolivier <lolivier@et.esiea.fr>
2023-06-12 19:44:20 +0200

    update files

--- a.py
+++ a.py
@@ -1,27 +1,75 @@
-# import kivy
-# from kivy.app import App
-# from kivy.lang import Builder
-# from kivy.uix.boxlayout import BoxLayout
-# import time
-# from djitellopy import Tello
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.uix.togglebutton import ToggleButton
 
-# kivy.require('2.1.0')
+import time
+from djitellopy import tello
+import cv2
+cv2.__version__
 
-# class CameraClick(BoxLayout):
-#     def __init__(self, **kwargs):
-#         super(CameraClick, self).__init__(**kwargs)
-#         self.tello = Tello()
-#         self.tello.connect()
-#         self.tello.streamon()
+kivy.require('2.1.0')
+kivy; print(kivy.__version__)
 
-#     def capture(self):
-#         camera = self.ids['camera']
-#         timestr = time.strftime("%Y%m%d_%H%M%S")
+class CameraClick(BoxLayout):
+    def capture(self):
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        self.tello = tello.Tello()
+        #self.image = Image()
+        self.texture = None
+        layout = BoxLayout()
+        print("oui")
+        while True:
+            frame = self.tello.get_frame_read().frame
+            
+            self.texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            self.texture.blit_buffer(frame.tobytes(), colorfmt='bgr', bufferfmt='ubyte')
+            self.image.texture = self.texture
+            # self.ids.image_widget.texture = self.texture
+            image_widget = Image(texture=self.texture)
+            layout.add_widget(image_widget)
+            return layout
+            # layout.image_widget.texture = self.texture
+            # #self.layout.clear_widgets()
+            # self.add_widget(layout.image_widget)
+            # self.ids.image_widget.texture = self.image_widget
+            # return layout
+            
+    
 
-# class atest(App):
-#     def build(self):
-#         camera_click = CameraClick(drone=self.tello)
-#         return camera_click
 
-# testApp = atest()
-# testApp.run()
+
+
+class MainApp(App):
+    def build(self):
+        return CameraClick()
+    
+
+    def on_start(self):
+        print("start")
+        self.tello = tello.Tello()
+        self.tello.connect()
+        self.tello.streamon()
+
+        
+
+    def on_stop(self):
+        self.tello.streamoff()
+        self.tello.end()
+
+    # def onoff(self, instance):
+    #     self.tello = tello.Tello()
+    #     if instance.text=='Start':
+    #         self.tello.connect()
+    #         self.tello.streamon()
+    #         instance.text='Stop'
+    #     else:
+    #         self.tello.streamoff()
+    #         self.tello.end()
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-
 from djitellopy import tello
 import time
 import cv2
@@ -19,14 +18,6 @@ me.streamoff()
 me.streamon()
 me.takeoff()
 
-# temp = 0
-# while (temp < 8):
-#     me.rotate_clockwise(45)
-#     time.sleep(1)
-#     temp=temp+1
-
-# me.move_forward(60)
-
 
 def rotation():
         me.rotate_clockwise(90)
@@ -37,51 +28,73 @@ def rotation():
 def camera():    
     n = 0
     temp = 0
+
+    minLength = 125
+
+    typesOpt = {
+        "names" : {
+            "LINES" : 0,
+            "FILTERED_LINES" : 1,
+            "LONG_LINES" : 2
+        },
+        "values" : [
+            "lines",
+            "filtered_lines",
+            "long_lines"
+        ],
+        "criterias" : {
+            "MIN_LENGTH" : 100,
+            "MAX_VERTICAL_POSITION" : 200,
+            "MAX_CENTER_DISTANCE" : 200,
+            "MIN_ANGLE" : 1
+        },
+        "filters" : {
+            "LENGTH" : 0,
+            "FORWARD" : 1,
+            "BOTTOM" : 2
+        }
+    }
+
+    firstVerticalDistance = 0
+    n = 0
+
     while True:
         img = me.get_frame_read().frame
         img = cv2.resize(img, (640, 480))
         cv2.imshow("Image", img)
-        
 
-        # typesOpt = {
-        #     "names" : {
-        #         "LINES" : 0,
-        #         "FILTERED_LINES" : 1,
-        #         "LONG_LINES" : 2
-        #     },
-        #     "values" : [
-        #         "lines",
-        #         "filtered_lines",
-        #         "long_lines"
-        #     ],
-        #    "criterias" : {
-        #         "MIN_LENGTH" : 120,
-        #         "MAX_VERTICAL_POSITION" : 120,
-        #         "MAX_CENTER_DISTANCE" : 150,
-        #         "MIN_ANGLE" : 1
-        #     },
-        #     "filters" : {
-        #         "LENGTH" : 0,
-        #         "FORWARD" : 1,
-        #         "BOTTOM" : 2
-        #     }
-        # }
+        r, line, m = detect_line(img, typesOpt['filters']['FORWARD'], minLength)
+        if line is not None :
+            print(f"Tracking line : {line}")
 
-        # r, line = detect_line(img, typesOpt['filters']['FORWARD'])
-        # if line is not None :
-        #     print(f"Tracking line : {line}")
-        #     moves = line['moves']
-        #     mx = moves['horizontalMove']
-        #     my1 = moves['firstVerticalDistance']
-        #     my2 = moves['verticalDestination']
-        #     cv2.imshow("Image", r)
-        #     time.sleep(2)
-        #     me.move_forward(150)
+            n += 1
+
+            cv2.imshow("Image", r)
             
-        
+            mx1Cm, my1Cm, my2Cm = m
+            if mx1Cm < 0 :
+                # Move on the LEFT
+                me.move_left(-mx1Cm)
+            elif mx1Cm > 0 :
+                # Move on the RIGHT
+                me.move_right(mx1Cm)
+            
+            me.move_forward(my1Cm)
+            me.move_forward(my2Cm)
+
+            if minLength > 0 :
+                if my2Cm == line['moves']['verticalDestination'] :
+                    minLength -= 30
+            
+            print(f"Minimum length : {minLength}")
+            time.sleep(1)  
+
+        elif n != 0 :
+            n = 0
+            me.move_forward(80)                  
+
         if cv2.waitKey(5) & 0xFF == ord('q'):
             break
-        #time.sleep(1)
 
     
         
@@ -89,20 +102,12 @@ def camera():
 
 started_evt = Event()
 y = threading.Thread(target=camera)
-x = threading.Thread(target=rotation)
-
 
 y.start()
 time.sleep(6)
-me.move_left(20)
 
 
    
-#me.move_forward(20)
-
-
-
-    
 
 cv2.destroyAllWindows()
 
